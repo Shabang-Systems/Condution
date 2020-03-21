f5bdf7439bb7ba1118aeedd0131e28ad34bdb489
Houjun Liu <hliu@shabang.cf>
2020-03-21 09:19:28 -0700

    Refactoring main.js....

--- main.js
+++ main.js
@@ -1,23 +1,25 @@
 const { app, BrowserWindow } = require('electron')
 
 function createWindow () {
-  // Create the browser window.
-  let win = new BrowserWindow({
-    width: 800,
-    height: 600,
-    title: "Conduction",
-    webPreferences: {
-      nodeIntegration: true
-    },
-    titleBarStyle: 'hiddenInset'
-  })
-  win.on('page-title-updated', function(e) {
-    e.preventDefault()
-  });
+    // Create the browser window.
+    let win = new BrowserWindow({
+        'width': 800,
+        'height': 600,
+        'minWidth': 600,
+        'minHeight': 400,
+        'title': "Conduction",
+        'webPreferences': {
+            'nodeIntegration': true
+        },
+        'titleBarStyle': 'hiddenInset'
+    })
+    win.on('page-title-updated', function(e) {
+        e.preventDefault()
+    });
 
-  // and load the index.html of the app.
-  win.loadFile('index.html')
+    // and load the index.html of the app.
+    win.loadFile('index.html')
 }
 
-app.setName('Condution');
+app.name = 'Condution';
 app.whenReady().then(createWindow)
