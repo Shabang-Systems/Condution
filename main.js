4889dc63f377b506c54ef212de7a9a155331394b
Houjun Liu <hliu@shabang.cf>
2020-03-20 23:11:23 -0700

    Working Header!!

--- main.js
+++ main.js
@@ -5,14 +5,19 @@ function createWindow () {
   let win = new BrowserWindow({
     width: 800,
     height: 600,
+    title: "Conduction",
     webPreferences: {
       nodeIntegration: true
     },
     titleBarStyle: 'hiddenInset'
   })
+  win.on('page-title-updated', function(e) {
+    e.preventDefault()
+  });
 
   // and load the index.html of the app.
   win.loadFile('index.html')
 }
 
+app.setName('Condution');
 app.whenReady().then(createWindow)
