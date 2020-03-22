cc3ebbc0743a912c569d9215aaa0518fa43b49ff
Houjun Liu <hliu@shabang.cf>
2020-03-21 17:13:35 -0700

    Page Handling! Ability to toggle pages, yay!

---
+++ js/index.js
@@ -0,0 +1,38 @@
+function showPage(pageId){
+    $("#"+pageId).css("display", "block");
+    $("#content-area").children().each(function(){
+        let item = $(this)
+        if (item.attr("id") != pageId){
+           item.css("display", "none")
+       }
+    });
+}
+
+var active = "today"
+
+$(document).on('click', '.perspective', function(e) {
+    $("#"+active).removeClass('today-highlighted perspective-selected')
+    showPage("perspective-page")
+    active = $(this).attr('id')
+    if (active.includes("today")){
+        $("#"+active).addClass("today-highlighted")
+    } else if (active.includes("perspective")){
+        $("#"+active).addClass("perspective-selected")
+    }
+
+    //console.log($(this).attr("id"));
+    
+})
+
+$(document).on('click', '.today', function(e) {
+
+    $("#"+active).removeClass('today-highlighted perspective-selected')
+    showPage("upcoming-page")
+    active = $(this).attr('id')
+    if (active.includes("today")){
+        $("#"+active).addClass("today-highlighted")
+    } else if (active.includes("perspective")){
+        $("#"+active).addClass("perspective-selected")
+    }
+    //console.log($(this).attr("id"));
+})
