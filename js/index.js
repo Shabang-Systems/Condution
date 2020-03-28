e6af2841adfb4e66e335d7de20c32a557fe594ce
Houjun Liu <hliu@shabang.cf>
2020-03-28 16:11:28 -0700

    Finished Task Edit View! As in, tags!

--- js/index.js
+++ js/index.js
@@ -1,3 +1,25 @@
+var substringMatcher = function(strs) {
+  return function findMatches(q, cb) {
+    var matches, substringRegex;
+
+    // an array that will be populated with substring matches
+    matches = [];
+
+    // regex used to determine if a string contains the substring `q`
+    substrRegex = new RegExp(q, 'i');
+
+    // iterate through the pool of strings and for any string that
+    // contains the substring `q`, add it to the `matches` array
+    $.each(strs, function(i, str) {
+      if (substrRegex.test(str)) {
+        matches.push(str);
+      }
+    });
+
+    cb(matches);
+  };
+};
+
 function showPage(pageId){
     $("#"+pageId).css("display", "block");
     $("#content-area").children().each(function(){
@@ -62,5 +84,23 @@ $('.datebox').datetimepicker({
     timeFormat: "hh:mm tt",
 	showHour: false,
 	showMinute: false,
+});
+
+var states = ['Alabama', 'Alaska', 'Arizona', 'Arkansas', 'California',
+  'Colorado', 'Connecticut', 'Delaware', 'Florida', 'Georgia', 'Hawaii',
+  'Idaho', 'Illinois', 'Indiana', 'Iowa', 'Kansas', 'Kentucky', 'Louisiana',
+  'Maine', 'Maryland', 'Massachusetts', 'Michigan', 'Minnesota',
+  'Mississippi', 'Missouri', 'Montana', 'Nebraska', 'Nevada', 'New Hampshire',
+  'New Jersey', 'New Mexico', 'New York', 'North Carolina', 'North Dakota',
+  'Ohio', 'Oklahoma', 'Oregon', 'Pennsylvania', 'Rhode Island',
+  'South Carolina', 'South Dakota', 'Tennessee', 'Texas', 'Utah', 'Vermont',
+  'Virginia', 'Washington', 'West Virginia', 'Wisconsin', 'Wyoming'
+];
+
+$('.task-tag').tagsinput({
+  typeaheadjs: {
+    name: 'states',
+    source: substringMatcher(states)
+  }
 });
 
