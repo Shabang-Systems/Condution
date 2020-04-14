73ae052e68ac5467cb36560e7e1fbc91ccdb9d6e
Houjun Liu <hliu@shabang.cf>
2020-04-14 12:46:22 -0700

    Reverting @Ban-Ironic-Ohms's Misleading changes
    Window structure reverted for macOS
    
    Co-authored-by: Micah Robinson <micahrobinson93@gmail.com>

--- main.js
+++ main.js
@@ -12,7 +12,7 @@ function createWindow () {
         'webPreferences': {
             'nodeIntegration': true
         },
-        'titleBarStyle': 'hiddenInsert',
+        'titleBarStyle': 'hiddenInset',
         'frame': false,
         'transparent': true,
     })
