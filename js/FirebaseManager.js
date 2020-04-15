8cf6415948fb7ed3870e1aa856979c8a93ee1409
Exr0n <spotyie@gmail.com>
2020-04-14 18:43:25 -0700

    NW: refactoring FirebaseManager

--- js/FirebaseManager.js
+++ js/FirebaseManager.js
@@ -1,6 +1,6 @@
 
 async function getTasks(userID) {
-    return dbGet({users: userID, tasks: undefined}) // TODO: replace with cRef.get()
+    return cRef("users", userID, "tasks").get()
     .then(snap => snap.docs
         .map(doc => doc.id)
     ).catch(err => {
@@ -9,19 +9,25 @@ async function getTasks(userID) {
 }
 
 async function getInboxTasks(userID) {
-    let inboxDocs = await dbGet({users: userID, tasks: [['project', '==', ''], ['isComplete', "==", false]]}) // TODO: replace with cRef.get()
-        .then(snap => snap.docs
-    ).catch(err => {
-        console.error('Error getting documents', err);
-    });
-    inboxDocs.sort((a,b) => a.data().order - b.data().order);
+    let inboxDocs = await cRef(
+        "users", userID,
+        "tasks",
+            ['project', '==', ''],
+            ['isComplete', "==", false])
+        .get()
+        .then(snap => snap.docs)
+        .catch(err => {
+            console.error('Error getting documents', err);
+        });
+    inboxDocs.sort((a,b) => a.data().order - b.data().order);   // TODO: use firebase native ordering
     return inboxDocs.map(doc => doc.id);
 }
 
 async function getDSTasks(userID) {
     let dsTime = new Date(); // TODO: merge with next line?
     dsTime.setHours(dsTime.getHours() + 24);
-    return dbGet({users: userID, tasks: [['due', '<=', dsTime], ['isComplete', "==", false]]}) // TODO: replace with cRef.get()
+    return cRef("users", userID,
+        "tasks", ['due', '<=', dsTime], ['isComplete', "==", false])
     .then(snap => snap.docs
         .map(doc => doc.id)
     ).catch(console.error);
@@ -35,16 +41,16 @@ async function getInboxandDS(userID) {
 }
 
 async function getTaskInformation(userID, taskID) {
-    return (await dbGet({users: userID, tasks: taskID})).data(); // TODO: replace with cRef.get()
+    return (await cRef("users", userID, "tasks", taskID).get()).data();
 }
 
 async function getProjectsandTags(userID) {
     // NOTE: no longer console.error when  !project/tag.exists
     let projectIdByName = {};
     let projectNameById = {};
-    await dbGet({users: userID, projects: undefined}) // TODO: replace with cRef.get()
+    await cRef("users", userID, "projects").get()   // TODO: combine database hits
         .then(snap => snap.docs.forEach(proj => {
-            dbGet({users: userID, projects: proj.id}) // TODO: replace with cRef.get()
+            cRef("users", userID, "projects", proj.id)
                 .then(proj => {
                     if (proj.exists) {
                         projectNameById[proj.id] = proj.data().name;
@@ -56,9 +62,9 @@ async function getProjectsandTags(userID) {
 
     let tagIdByName = {};
     let tagNameById = {};
-    await dbGet({users: userID, tags: undefined}) // TODO: replace with cRef.get()
+    await cRef("users", userID, "tags").get()
         .then(snap => snap.docs.forEach(tag => {
-            dbGet({users: userID, tags: tag.id}) // TODO: replace with cRef.get()
+            cRef("users", userID, "tags", tag.id).get()
                 .then(tag => {
                     if (tag.exists) {
                         tagNameById[tag.id] = tag.data().name;
@@ -73,13 +79,13 @@ async function getProjectsandTags(userID) {
 
 async function modifyTask(userID, taskID, updateQuery) {
     // TODO: untested
-    dbGet({users: userID, tasks: taskID}) // TODO: replace with cRef.get()
+    cRef("users", userID, "tasks", taskID).get()
         .then((doc) => { // TODO: create a doc exists? wrapper
             if (doc.exists !== true)
                 throw "excuse me wth, why are you getting me to modify something that does not exist???? *hacker noises*";
         });
 
-    (await dbRef({users: userID, tasks: taskID})) // TODO: replace with cRef
+    await cRef("users", userID, "tasks", taskID)
         .update(updateQuery)
         .catch(console.error);
 }
@@ -95,39 +101,39 @@ async function newTask(userID, taskObj) { //TODO: task order calculation
         taskObj.order = projL;
     }
 
-    return (await dbRef({users: userID, tasks: undefined}).add(taskObj)).id; // TODO: replace with cRef
+    return cRef("users", userID, "tasks").add(taskObj).id;
 }
 
 async function newTag(userID, tagName) {
-    return (await dbRef({users: userID, tags: undefined}).add({name: tagName})).id; // TODO: replace with cRef
+    return cRef("users", userID, "tags").add({name: tagName}).id;
 }
 
 async function completeTask(userID, taskID) {
-    await dbGet({users: userID, tasks: taskID}) // TODO: replace with cRef.get()
+    await cRef("users", userID, "tasks", taskID).get()
         .then(doc => {
             if (doc.exists !== true) {
                 throw "Document not found. Please don't try to set documents that don't exist.";
             }
         });
-    await dbRef({users: userID, tasks: taskID}).update({ // TODO: replace with cRef
+    await cRef("users", userID, "tasks", taskID).update({
             isComplete: true
         });
 }
 
 async function deleteTask(userID, taskID) {
-    (await dbRef({users: userID, tasks: taskID})).delete() // TODO: replace with cRef
+    await cRef("users", userID, "tasks", taskID).delete()
         .then(() => {console.log("Task successfully deleted!")})
         .catch(console.error);
 }
 
 async function deleteProject(userID, projectID) {
-    (await dbRef({users: userID, projects: projectID})).delete() // TODO: replace with cRef
+    await cRef("users", userID, "projects", projectID).delete()
         .then(() => {console.log("Project successfully deleted!")})
         .catch(console.error);
 }
 
 async function deleteTag(userID, tagID) {
-    (await dbRef({users: userID, tags: tagID})).delete() // TODO: replace with cRef
+    await cRef("users", userID, "tags", tagID).delete()
         .then(() => {console.log("Tag successfully deleted!")})
         .catch(console.error);
 }
@@ -135,14 +141,15 @@ async function deleteTag(userID, tagID) {
 async function getProjectStructure(userID, projectID) {
     let children = [];
 
-    await dbGet({users:userID, projects:projectID, children:undefined}).then(snapshot => { // TODO: replace with cRef.get()
-        snapshot.docs.forEach(async doc => {                 //  for each child
+    await cRef("users", userID, "projects", projectID, "children").get()
+        .then(snapshot => {snapshot.docs
+                .forEach(async doc => {                 //  for each child
             if (doc.data().type === "task") { // TODO combine these if statements
-                let order = (await dbGet({users:userID, tasks:(doc.data().childrenID)})).data().order;//.collection("users").doc(userID).collection("tasks").doc(doc.data().childrenID).get()).data().order; //  get the order of the task // TODO: replace with cRef.get()
+                let order = (await cRef("users", userID, "tasks", doc.data().childrenID).get()).data().order;//.collection("users").doc(userID).collection("tasks").doc(doc.data().childrenID).get()).data().order; //  get the order of the task // TODO: replace with cRef.get()
                 children.push({type: "task", content: doc.data().childrenID, sortOrder: order});   //  push its ID to the array
             } else if (doc.data().type === "project") {    //      if the child is a project
                 // push the children of this project---same structure as the return obj of this func
-                let order = (await dbGet({users:userID, projects:(doc.data().childrenID)})).data().order;//.collection("users").doc(userID).collection("projects").doc(doc.data().childrenID).get()).data().order; //  get the order of theproject // TODO: replace with cRef.get()
+                let order = (await cRef("users", userID, "projects", (doc.data().childrenID).get())).data().order;//.collection("users").doc(userID).collection("projects").doc(doc.data().childrenID).get()).data().order; //  get the order of theproject // TODO: replace with cRef.get()
                 children.push({type: "project", content: (await getProjectStructure(userID, doc.data().childrenID)), sortOrder: order});
             }
         });
