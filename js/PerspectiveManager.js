55ec54325bf51ae9cb0f901d1a0e9f59a6e2b687
Houjun Liu <hliu@shabang.cf>
2020-04-19 21:05:45 -0700

    NW: implimented Parse Logic Group

--- js/PerspectiveManager.js
+++ js/PerspectiveManager.js
@@ -10,18 +10,6 @@ const perspective = function(){
 
     let parseTaskCaptureGroup = (str) => str.split("$");
 
-    let compileTask = function(str, pPaT) {
-        let queries = []
-        str.match(cgs.taskFilter).forEach(function(e) {
-            if (e[0] == "!") {
-                e.includes(".") ? queries.push(['project', '!=',  pPaT[0][1][e.slice(1, e.length)]]) : queries.push(['tags', '!has', pPaT[1][1][e.slice(1, e.length)]]);
-            } else {
-                e.includes(".") ? queries.push(['project', '==',  pPaT[0][1][e.slice(1, e.length)]]) : queries.push(['tags', 'has', pPaT[1][1][e.slice(1, e.length)]]);
-            }
-        });
-        return getTasksWithQuery(uid, util.select.all(queries))
-    }
-
     let parseSpecialVariables = function(...val) {
         let vr;
         switch(val[0]) {
@@ -32,6 +20,51 @@ const perspective = function(){
         return vr;
     }
 
+    let compileTask = async function(uid, str, pPaT) {
+        let queries = []
+        str.match(cgs.taskFilter).forEach(function(e) {
+            if (e[0] == "!") {
+                e.includes(".") ? queries.push(['project', '!=',  pPaT[0][1][e.slice(1, e.length)]]) : queries.push(['tags', '!has', pPaT[1][1][e.slice(1, e.length)]]);
+            } else {
+                e.includes(".") ? queries.push(['project', '==',  pPaT[0][1][e.slice(1, e.length)]]) : queries.push(['tags', 'has', pPaT[1][1][e.slice(1, e.length)]]);
+            }
+        });
+        return await getTasksWithQuery(uid, util.select.all(queries))
+    }
+
+    let compileLogicCapGroup = async function(uid, tasks, cmp, value, ltr=true) {
+        let taskInfo = tasks[0].map(t=>await getTaskInformation(uid, t));
+        let taskCompValues;
+        // TODO: add more?
+        if (tasks[1].includes("due")) {
+            taskCompValues = taskInfo.map(t=>[t, t.due]);
+        } else if (tasks[1].includes("defer")) {
+            taskCompValues = taskInfo.map(t=>[t, t.defer]);
+        }
+        let filteredResults;
+        let util_datesequal = function(dateA, dateB) {
+            return dateA.getFullYear() === dateB.getFullYear() && dateA.getMonth() === dateB.getMonth() && dateA.getDate() === dateB.getDate();
+        }
+        switch (cmp) {
+            case "=":
+                taskCompValues = taskCompValues.filter(t=>util_datesequal(t[1], value));
+                break;
+            case ">":
+                ltr ? taskCompValues = taskCompValues.filter(t=>t[1]>value) : taskCompValues = taskCompValues.filter(t=>t[1]<value);
+                break;
+            case "<":
+                ltr ? taskCompValues = taskCompValues.filter(t=>t[1]<value) : taskCompValues = taskCompValues.filter(t=>t[1]>value);
+                break;
+            case ">=":
+                ltr ? taskCompValues = taskCompValues.filter(t=>t[1]>=value) : taskCompValues = taskCompValues.filter(t=>t[1]<=value);
+                break;
+            case "<=":
+                ltr ? taskCompValues = taskCompValues.filter(t=>t[1]<=value) : taskCompValues = taskCompValues.filter(t=>t[1]>=value);
+                break;
+
+        }
+        return taskCompValues.map(t=>t[0]);
+    }
 
     let getPerspectiveFromString = async function(uid, pStr) {
         let logicParsedGroups = []
@@ -44,10 +77,10 @@ const perspective = function(){
                 lhs, rhs = [parseTaskCaptureGroup(lhs), parseTaskCaptureGroup(rhs)];
 
                 if (lhs.test(taskCaptureGroup)) {
-                    lhs = [await compileTask(lhs[0], pPaT), lhs[1]];
+                    lhs = [await compileTask(uid, lhs[0], pPaT), lhs[1]];
                     rhs = parseSpecialVariables(rhs[1]);
                 } else {
-                    rhs = [await compileTask(rhs[0], pPaT), rhs[1]];
+                    rhs = [await compileTask(uid, rhs[0], pPaT), rhs[1]];
                     lhs = parseSpecialVariables(lhs[1]);
                 }
             } else {
