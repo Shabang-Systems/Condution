d9ee051104165baa43b204f1f800d1d754815f70
Exr0n <spotyie@gmail.com>
2020-04-28 16:48:27 -0700

    sync

--- js/APIServer.js
+++ js/APIServer.js
@@ -1,5 +1,5 @@
-console.log('loading express...');
 const port = 32445;
+
 const express = require('express');
 const app = express();
 
@@ -7,5 +7,5 @@ app.get('/', (req, res) => {
 	res.end('See https://github.com/Shabang-Systems/Condution/wiki/Localhost-API for more information.');
 });
 
-console.log(express);
+app.listen(port, () => { console.log(`API Server listening on port ${port}`); });
 
