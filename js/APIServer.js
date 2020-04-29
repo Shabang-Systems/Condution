c22629f2a7fccb17a9747cc83c9dfa86287b8676
Exr0n <spotyie@gmail.com>
2020-04-28 21:32:52 -0700

    set up app.post, need to namespace FbM.js

--- js/APIServer.js
+++ js/APIServer.js
@@ -7,5 +7,13 @@ app.get('/', (req, res) => {
 	res.end('See https://github.com/Shabang-Systems/Condution/wiki/Localhost-API for more information.');
 });
 
+app.post('/', (req, res) => {
+	let data = '';
+	req.on('data', chunk => data += chunk);
+	req.on('end', () => {
+		// TODO: check if it's a valid FirebaseManager function
+	});
+});
+
 app.listen(port, () => { console.log(`API Server listening on port ${port}`); });
 
