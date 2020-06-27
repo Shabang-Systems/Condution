5a62be6f7f92414571f7438bf660ecee941caa57
Houjun Liu <hliu@shabang.cf>
2020-06-26 22:46:44 -0700

    Notarizing difference?

--- build/buildscripts/aftersign.js
+++ build/buildscripts/aftersign.js
@@ -1,18 +1,34 @@
-require('dotenv').config();
-const { notarize } = require('electron-notarize');
+const fs = require('fs');
+const path = require('path');
+var electron_notarize = require('electron-notarize');
 
-exports.default = async function notarizing(context) {
-  const { electronPlatformName, appOutDir } = context;  
-  if (electronPlatformName !== 'darwin') {
-    return;
-  }
+module.exports = async function (params) {
+    // Only notarize the app on Mac OS only.
+    if (process.platform !== 'darwin') {
+        return;
+    }
+    console.log('afterSign hook triggered', params);
 
-  const appName = context.packager.appInfo.productFilename;
+    // Same appId in electron-builder.
+    let appId = 'cf.shabang.condution'
 
-  return await notarize({
-    appBundleId: 'cf.shabang.condution',
-    appPath: `${appOutDir}/${appName}.app`,
-    appleId: process.env.APPLEID,
-    appleIdPassword: process.env.APPLEIDPASS,
-  });
+    let appPath = path.join(params.appOutDir, `${params.packager.appInfo.productFilename}.app`);
+    if (!fs.existsSync(appPath)) {
+        throw new Error(`Cannot find application at: ${appPath}`);
+    }
+
+    console.log(`Notarizing ${appId} found at ${appPath}`);
+
+    try {
+        await electron_notarize.notarize({
+            appBundleId: appId,
+            appPath: appPath,
+            appleId: process.env.appleId,
+            appleIdPassword: process.env.appleIdPassword,
+        });
+    } catch (error) {
+        console.error(error);
+    }
+
+    console.log(`Done notarizing ${appId}`);
 };
