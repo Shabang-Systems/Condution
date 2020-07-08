c2b50b84a1826fc06d9d453a57b748acfb84ea36
Houjun Liu <hliu@shabang.cf>
2020-07-07 20:18:07 -0700

    Turning this into a single view app

--- src/js/app.js
+++ src/js/app.js
@@ -2088,21 +2088,174 @@ $(document).ready(async function() {
                 await ui.constructSidebar();
                 await ui.load("upcoming-page");
                 $("#loading").fadeOut();
-                /*currentTheme = "condutiontheme-default-dark";*/
-                //$("body").removeClass();
-                /*$("body").addClass(currentTheme);*/
+                $("#auth-content-wrapper").fadeOut();
                 $("#content-wrapper").fadeIn();
                 setInterval(() => {ui.update()}, 60 * 1000);
                 setInterval(()=> {ipcRenderer.send("updatecheck")}, 15*60*1000);
             } else {
-                window.location.replace("auth.html");
+                $("#content-wrapper").fadeOut();
+                user.sendEmailVerification();
+                $('#auth-left-menu').fadeIn();
+                $('#need-verify').fadeIn();
+                $('#recover-password').fadeOut();
+                $("#loading").fadeOut();
+                $("#authwall").fadeIn();
+                $("#auth-content-wrapper").fadeIn();
             }
         } else {
             firebase.auth().signOut();
-            window.location.replace("auth.html");
+            $("#content-wrapper").fadeOut();
+            $("#loading").fadeOut();
+            $("#authwall").fadeIn();
+            $('#auth-left-menu').fadeIn();
+            $("#auth-content-wrapper").fadeIn();
+
         }
     });
 });
+(function() {
+    let mode = "login";
+    /*lottie.loadAnimation({*/
+        //container: $("#loading-anim")[0],
+        //renderer: 'svg',
+        //autoplay: true,
+        //loop: true,
+        //path: 'static/loadanim_final.json'
+    //})
+    /*$("#loading").hide().css("display", "flex").fadeIn();*/
+    // Initialize Firebase Application
+    // TODO TODO TODO !!!! Change this on deploy
+
+    let isNASuccess = false;
+
+    let auth = function() {
+        if (isNASuccess) {
+            var user = firebase.auth().currentUser;
+            user.updateProfile({displayName: $("#name").val()});
+            // TODO: other wonderful onboarding things
+            $("#auth-content-wrapper").fadeOut();
+            isNASuccess = false;
+        }
+        firebase.auth().signInWithEmailAndPassword($("#email").val(), $("#password").val()).then(function() {
+            if (firebase.auth().currentUser.emailVerified){
+                $("#auth-content-wrapper").fadeOut();
+            } else {
+                firebase.auth().currentUser.sendEmailVerification();
+                $('#auth-left-menu').fadeIn();
+                $('#need-verify').fadeIn();
+                $('#recover-password').fadeOut();
+                $("#authwall").fadeIn();
+            }
+        }).catch(function(error) {
+            // Handle Errors here.
+            console.log(error);
+            console.log("Silly goose");
+            const errorCode = error.code;
+            const errorMessage = error.message;
+            $(".auth-upf").addClass("wrong");
+        });
+    };
+
+    let rec = function() {
+        firebase.auth().sendPasswordResetEmail($("#email").val()).then(function() {
+            $(".auth-upf").removeClass("wrong");
+            $("#password").show();
+            $("#newuser").html("Make an account.");
+            $("#newuser").show();
+            $("#recover-password").html("Recover Password");
+            $("#greeting-auth-normal").html("Let's authenticate. Otherwise this may not be useful...");
+            $('#recover-password').fadeOut();
+            $('#need-verify').fadeIn();
+        }).catch(function(error) {
+            $(".auth-upf").addClass("wrong");
+        });
+    }
+
+    let nu = function() {
+        firebase.auth().createUserWithEmailAndPassword($("#email").val(), $("#password").val()).catch(function(error) {
+            console.log("Silly goose");
+        });
+        $('#need-verify').fadeIn();
+        $('#recover-password').fadeOut();
+        isNASuccess = true;
+    }
+
+    $("#password").keydown(function(e) {
+        if (e.keyCode == 13) {
+            switch (mode) {
+                case "login":
+                    auth();
+                    break;
+                case "newuser":
+                    nu();
+                    break;
+            }
+        }
+    });
+
+    $("#login").click(function(e) {
+        switch (mode) {
+            case "login":
+                auth();
+                break;
+            case "newuser":
+                nu();
+                break;
+            case "recover":
+                rec();
+                break;
+        }
+    });
+
+    $("#recover-password").click(function(e) {
+        switch (mode) {
+            case "login":
+                $("#password").hide();
+                $("#recover-password").html("Remembered? Login");
+                $("#newuser").hide();
+                $("#greeting-auth-normal").html("No worries! Let's recover your password.");
+                mode = "recover";
+                break;
+            case "newuser":
+                $("#name-tray").hide();
+                $("#password").hide();
+                $("#recover-password").html("Remembered? Login");
+                $("#newuser").hide();
+                $("#greeting-auth-normal").html("No worries! Let's recover your password.");
+                mode = "recover";
+                break;
+            case "recover":
+                $("#password").show();
+                $("#newuser").html("Make an account.");
+                $("#newuser").show();
+                $("#recover-password").html("Recover Password");
+                $("#greeting-auth-normal").html("Let's authenticate. Otherwise this may not be useful...");
+                mode = "login";
+        }
+    });
+
+    $("#newuser").click(function(e) {
+        switch (mode) {
+            case "login":
+                $("#name-tray").slideDown(300);
+                $(this).html("Sign in.");
+                mode = "newuser";
+                $("#greeting-auth-normal").html(`Welcome aboard! It is possible that we will loose your data...`);
+                break;
+            case "newuser":
+                $("#name-tray").slideUp(300);
+                $(this).html("Make an account.");
+                $("#greeting-auth-normal").html("Let's authenticate. Otherwise this may not be useful...");
+                mode = "login";
+                break;
+        }
+    });
+
+    const greetings = ["Hello there!", "Hey!", "G'day!", "What's up!", "Howdy!", "Yo!"];
+    $("#greeting-auth").html(greetings[Math.floor(Math.random() * greetings.length)]);
+
+})();
+
 console.log('%cSTOP! ', 'background: #fff0f0; color: #434d5f; font-size: 80px');
 console.log('%cClose this panel now.', 'background: #fff0f0;color: black;'+css);
 console.log('%c19/10 chance you are either a terribly smart person and should work with us (hliu@shabang.cf) or are being taken advantanged of by a very terrible person. ', 'background: #fff0f0; color: #434d5f; font-size: 20px');
