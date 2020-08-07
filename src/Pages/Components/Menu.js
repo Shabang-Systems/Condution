ac16d47e6393f86b267bdefcd010169b171034ab
Houjun Liu <hliu@shabang.cf>
2020-08-07 13:36:25 -0700

    More menu stuff

--- src/Pages/Components/Menu.js
+++ src/Pages/Components/Menu.js
@@ -17,7 +17,7 @@ class Menu extends Component {
     constructor(props) {
         super(props);
 
-        this.state = {};
+        this.state = {currentSelection: "today"};
 
         autoBind(this);
     }
@@ -26,17 +26,21 @@ class Menu extends Component {
         return (
             <div className="menu-area">
                 <div>
-                    <div id="today" className="today today-highlighted"><i className="fas fa-chevron-circle-right"></i><span style={{paddingLeft:"8px"}} id="upcoming-text-name">Upcoming</span></div>
-                    <div id="completed" className="today"><i className="fas fa-check-circle"></i><span style={{paddingLeft:"8px"}} id="completed-text-name">Completed</span></div>
+                    <div id="today" className={"today "+(this.state.currentSelection === "today" ? "today-highlighted": "")}><i className="fas fa-chevron-circle-right"></i><span style={{paddingLeft:"8px"}} id="upcoming-text-name">Upcoming</span></div>
+                    <div id="completed" className={"today "+(this.state.currentSelection === "completed" ? "today-highlighted": "")}><i className="fas fa-check-circle"></i><span style={{paddingLeft:"8px"}} id="completed-text-name">Completed</span></div>
                 </div>
                 <span className="menu-label">
                     <span id="perspectives-text-name">Perspectives</span> <div className="menu-subicon" id="perspective-add"><i className="fas fa-plus"></i></div>
                 </span>
-                <div className="perspectives menu-portion scrollable"></div>
+                <div className="perspectives menu-portion scrollable">
+                    HERE!
+                </div>
                 <span className="menu-label">
                     <span id="projects-text-name">Projects</span> <div className="menu-subicon" id="project-add-toplevel"><i className="fas fa-plus"></i></div>
                 </span>
-                <div className="projects menu-portion scrollable"></div>
+                <div className="projects menu-portion scrollable">
+                    HERE!
+                </div>
                 <div id="logout"><i className="fas fa-snowboarding" style={{paddingRight: "5px"}}></i><span id="logout-text-name">Logout</span></div>
             </div>
 
