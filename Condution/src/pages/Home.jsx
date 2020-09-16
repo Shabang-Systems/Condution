c2eef7ca9216b1fe5f569e2b75c8273a0678662e
Houjun Liu <hliu@shabang.cf>
2020-09-16 10:07:31 -0700

    Main

---
+++ Condution/src/pages/Home.jsx
@@ -0,0 +1,26 @@
+import { IonContent, IonHeader, IonPage, IonTitle, IonToolbar } from '@ionic/react';
+import React from 'react';
+import ExploreContainer from '../components/ExploreContainer';
+import './Home.css';
+
+const Home = () => {
+  return (
+    <IonPage>
+      <IonHeader>
+        <IonToolbar>
+          <IonTitle>Blank</IonTitle>
+        </IonToolbar>
+      </IonHeader>
+      <IonContent fullscreen>
+        <IonHeader collapse="condense">
+          <IonToolbar>
+            <IonTitle size="large">Blank</IonTitle>
+          </IonToolbar>
+        </IonHeader>
+        <ExploreContainer />
+      </IonContent>
+    </IonPage>
+  );
+};
+
+export default Home;
