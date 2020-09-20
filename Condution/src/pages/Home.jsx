75904267082e29aecc7b131d3fb33c5fe9e157d2
Houjun Liu <hliu@shabang.cf>
2020-09-20 16:22:49 -0700

    Navigation

--- Condution/src/pages/Home.jsx
+++ Condution/src/pages/Home.jsx
@@ -1,19 +1,45 @@
 import { IonContent, IonPage, IonSplitPane, IonMenu, IonText, IonIcon, IonMenuButton, IonRouterOutlet } from '@ionic/react';
 import { IonReactRouter } from '@ionic/react-router';
-import { Redirect, Route } from 'react-router-dom';
+import { Redirect, Route, Link } from 'react-router-dom';
 import { chevronForwardCircle, checkmarkCircle, filterOutline, listOutline } from 'ionicons/icons';
 import React, { Component } from 'react';
 import './Home.css';
+
+const autoBind = require('auto-bind/react');
+
+/* 
+ *
+ * Hello, human
+ * Good morning
+ * Read me please.
+ *
+ * Use the **state**'s itemSelected 
+ *     to manage the menu
+ * use the **React Router URL** 
+ *     to manage the page
+ *
+ * Otherwise, unreload-reload won't work.
+ *
+ * Thanks. @jemoka.
+ *
+ */
 
 class Home extends Component {
     constructor(props) {
         super(props);
 
-        this.state = {projects:[], perspectives:[]};
+        this.state = {projects:[], perspectives:[], itemSelected:{item:"upcoming", id:undefined}};
+
+        autoBind(this);
     }
 
     componentDidMount() {
         let view = this;
+        let uri = (new URL(document.URL)).pathname.split("/");
+        if (uri[1] === "")
+            this.setState({itemSelected:{item:"upcoming", id:""}});
+        else
+            this.setState({itemSelected:{item:uri[1], id:uri[2]}});
         (async function() {
             let tlp = await view.props.engine.db.getTopLevelProjects(view.props.uid);
             let psp = await view.props.engine.db.getPerspectives(view.props.uid);
@@ -23,26 +49,27 @@ class Home extends Component {
 
     render() {
     return (
-        <IonPage>
+    <IonPage>
+        <IonReactRouter>
             <IonContent>
-              <IonSplitPane id="main-split" contentId="main" when="md">
+              <IonSplitPane id="main-split" contentId="main" when="sm">
                 <IonMenu id="main-menu" contentId="main">
                     {/*
                     <div id="menu-hero">
                         Condution <span id="version-number" className="menu-decoration">v1.0.0</span>
                     </div>
                     */}
+                        
                     <br />
                     <IonContent>
                         {/* === Built Ins == */}
-                        <div className="menu-item menu-item-selected" style={{fontSize: 18}} ><IonIcon icon={chevronForwardCircle} />Upcoming</div>
-                        <div className="menu-item" style={{fontSize: 18}}><IonIcon icon={checkmarkCircle} />Completed</div>
-
+                        <Link to="/upcoming" onClick={()=>this.setState({itemSelected:{item:"upcoming", id:undefined}})}><div className={"menu-item "+(this.state.itemSelected.item === "upcoming" ? "menu-item-selected" : "")} style={{fontSize: 18}}><IonIcon icon={chevronForwardCircle} />Upcoming</div></Link>
+                            <Link to="/completed" onClick={()=>this.setState({itemSelected:{item:"completed", id:undefined}})}><div className={"menu-item "+(this.state.itemSelected.item === "completed" ? "menu-item-selected" : "")} style={{fontSize: 18}}><IonIcon icon={checkmarkCircle} />Completed</div></Link>
                         {/* === Perspectives == */}
                         <div className="menu-sublabel menu-decoration">Perspectives</div>
                             {/* === Perspective Contents == */}
                             {this.state.perspectives.map((psp) => {
-                                    return (<div className="menu-item" key={psp.id}><IonIcon icon={filterOutline} />{psp.name}</div>)
+                                return (<Link key={psp.id} to={`/perspectives/${psp.id}`} onClick={()=>this.setState({itemSelected:{item:"perspectives", id:psp.id}})}><div className={"menu-item "+(this.state.itemSelected.item === "perspectives" && this.state.itemSelected.id === psp.id ? "menu-item-selected" : "")}><IonIcon icon={filterOutline} />{psp.name}</div></Link>)
                             })}
 
 
@@ -50,21 +77,21 @@ class Home extends Component {
                         <div className="menu-sublabel menu-decoration">Projects</div>
                             {/* === Project Contents == */}
                             {this.state.projects.map((proj) => {
-                                    return ( <div className="menu-item" key={proj.id}><IonIcon icon={listOutline} />{proj.name}</div> )
+                                return (<Link key={proj.id} to={`/projects/${proj.id}`} onClick={()=>this.setState({itemSelected:{item:"projects", id:proj.id}})}><div className={"menu-item "+(this.state.itemSelected.item === "projects" && this.state.itemSelected.id === proj.id ? "menu-item-selected" : "")}><IonIcon icon={listOutline} />{proj.name}</div></Link> )
                             })}
                     </IonContent>
                 </IonMenu>
-
                 <IonContent id="main">
-                    <IonReactRouter>
                         <IonRouterOutlet>
                             {/*<Route path="/" component={Home} exact={true} />*/}
+                             <Route exact path="/" render={() => <Redirect to="/upcoming" />} />
+                                
                         </IonRouterOutlet>
-                    </IonReactRouter>
                 </IonContent>
               </IonSplitPane>
             </IonContent>
-        </IonPage>
+        </IonReactRouter>
+    </IonPage>
   );
     }
 };
