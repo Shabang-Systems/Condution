3b42129c4b8949bbb6eca43fe23c1b278e04216d
Houjun Liu <hliu@shabang.cf>
2020-09-24 20:35:19 -0700

    React spring!

--- Condution/src/pages/Components/Task.jsx
+++ Condution/src/pages/Components/Task.jsx
@@ -2,6 +2,8 @@ import { IonItem, IonInput, IonContent, IonGrid, IonRow, IonCol } from '@ionic/r
 //import { chevronForwardCircle, checkmarkCircle, filterOutline, listOutline, bicycle } from 'ionicons/icons';
 import React, { Component } from 'react';
 import './Task.css';
+import { Spring } from 'react-spring/renderprops'
+
 
 const autoBind = require('auto-bind/react');
 
@@ -13,15 +15,29 @@ class Task extends Component {
         this.state = {expanded: false}
     }
 
+    expand = () => this.setState(state => ({expanded: !state.expanded}));
+
     render() {
         return (
-            <div className="task collapsed">
-                <div style={{display: "inline-block"}}>
-                    <input type="checkbox" id={"task-check-"+this.props.id} className="task-check"/>
-                    <label className="task-pseudocheck" id={"task-pseudocheck-"+this.props.id} htmlFor={"task-check-"+this.props.id}>&zwnj;</label>
-                </div>
-                <input value={"apple pie"} placeholder={"Task Name"} onChange={()=>{}} className="task-name" type="text" autoComplete="off" placeholder="Task Name"></input>
-            </div>
+            <Spring
+                from={{x:0}}
+                to={{x:this.state.expanded?120:0}}
+            >
+            {props => {
+                return (
+                    <div className="task collapsed">
+                        <div style={{display: "inline-block", transform: "translateY(-1px)", marginLeft: props.x}}>
+                            <input type="checkbox" id={"task-check-"+this.props.id} className="task-check" onChange={()=>{
+                                this.expand();
+                                console.log("aoeu");
+                            }}/>
+                            <label className="task-pseudocheck" id={"task-pseudocheck-"+this.props.id} htmlFor={"task-check-"+this.props.id}>&zwnj;</label>
+                        </div>
+                        <input value={"apple pie"} placeholder={"Task Name"} onChange={()=>{}} className="task-name" type="text" autoComplete="off" placeholder="Task Name"></input>
+                    </div>
+                )}
+            } 
+            </Spring>
         )
     }
 }
