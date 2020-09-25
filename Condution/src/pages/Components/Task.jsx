a693bbb3626ce4d609d32ce4ef63b40336faacf0
Houjun Liu <hliu@shabang.cf>
2020-09-24 20:54:47 -0700

    Neon unicorn barf

--- Condution/src/pages/Components/Task.jsx
+++ Condution/src/pages/Components/Task.jsx
@@ -3,6 +3,7 @@ import { IonItem, IonInput, IonContent, IonGrid, IonRow, IonCol } from '@ionic/r
 import React, { Component } from 'react';
 import './Task.css';
 import { Spring } from 'react-spring/renderprops'
+import OutsideClickHandler from 'react-outside-click-handler';
 
 
 const autoBind = require('auto-bind/react');
@@ -15,29 +16,32 @@ class Task extends Component {
         this.state = {expanded: false}
     }
 
-    expand = () => this.setState(state => ({expanded: !state.expanded}));
+    toggleTask = () => this.setState(state => ({expanded: !state.expanded}));
 
     render() {
         return (
-            <Spring
-                from={{x:0}}
-                to={{x:this.state.expanded?120:0}}
+            <OutsideClickHandler
+
+                      onOutsideClick={this.toggleTask}
             >
-            {props => {
+            <Spring
+                from={{taskHeight:0, background:"red"}}
+                to={{taskHeight:this.state.expanded?250:0, background:this.state.expanded?"blue":"red"}}
+            >
+            {animatedProps => {
                 return (
-                    <div className="task collapsed">
-                        <div style={{display: "inline-block", transform: "translateY(-1px)", marginLeft: props.x}}>
-                            <input type="checkbox" id={"task-check-"+this.props.id} className="task-check" onChange={()=>{
-                                this.expand();
-                                console.log("aoeu");
-                            }}/>
+                    <div className={"task "+(this.state.expanded?"expanded":"collapsed")} style={{height: animatedProps.taskHeight, background: animatedProps.background}}>
+                        <div style={{display: "inline-block", transform: "translateY(-1px)"}}>
+                            <input type="checkbox" id={"task-check-"+this.props.id} className="task-check" onChange={()=>{console.log("OMOOB!")}}/>
                             <label className="task-pseudocheck" id={"task-pseudocheck-"+this.props.id} htmlFor={"task-check-"+this.props.id}>&zwnj;</label>
                         </div>
-                        <input value={"apple pie"} placeholder={"Task Name"} onChange={()=>{}} className="task-name" type="text" autoComplete="off" placeholder="Task Name"></input>
+                        <input value={"apple pie"} placeholder={"Task Name"} onChange={()=>{}} onClick={()=>{ if(!this.state.expanded) this.toggleTask() }} className="task-name" type="text" autoComplete="off" placeholder="Task Name"></input>
                     </div>
                 )}
             } 
             </Spring>
+                    </OutsideClickHandler>
+
         )
     }
 }
