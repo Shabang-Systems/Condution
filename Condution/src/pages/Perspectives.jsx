993fd4779fa0dd524b8f0bea659ab5ece4f12520
TheEnquirer <huxmarv@nuevaschool.org>
2020-10-16 22:27:49 -0700

    perspectives showing tasks!

--- Condution/src/pages/Perspectives.jsx
+++ Condution/src/pages/Perspectives.jsx
@@ -9,12 +9,72 @@ import Task from './Components/Task';
 const autoBind = require('auto-bind/react');
 
 class Perspectives extends Component {
+
+    constructor(props) {
+        super(props);
+
+        this.state = {
+	    taskList: [],
+	    possibleProjects:{}, // see jacks comments in upcoming 
+	    possibleTags:{}, 
+	    possibleProjectsRev:{}, 
+	    possibleTagsRev:{}, 
+	    availability: [], 
+	    projectSelects:[], 
+	    tagSelects: [], 
+	    projectDB: {}
+
+	};
+
+	
+        this.updatePrefix = this.random();
+        this.props.gruntman.registerRefresher((this.refresh).bind(this));
+
+        // AutoBind!
+        autoBind(this);
+    }
+
+    async refresh() {
+	let possiblePerspectives = await this.props.engine.db.getPerspectives(this.props.uid); // get all possible perspectives
+	let perspectiveObject = possiblePerspectives[0][this.props.id] // get the one we want based on page id 
+	let taskList = await this.props.engine.perspective.calc(this.props.uid, perspectiveObject.query, perspectiveObject.avail, perspectiveObject.tord) // then get the tasks from that perspective
+	this.setState({taskList: taskList})
+	console.log(this.state.taskList)
+
+    }
+
+    componentDidMount() {
+	this.refresh()
+
+    }
+
+
+    random() { return (((1+Math.random())*0x10000)|0).toString(16)+"-"+(((1+Math.random())*0x10000)|0).toString(16);}
+
     render() {
-        console.log(this.props);
+        //console.log(this.props.id);
         return (
             <IonPage>
                 <IonContent>
-                    DONT USE ME. I AM DUMB
+	            {this.state.taskList.map(id => (
+                        <Task 
+			tid={id}
+			key={id+"-"+this.updatePrefix} 
+			uid={this.props.uid} 
+			engine={this.props.engine} 
+			gruntman={this.props.gruntman} 
+			availability={this.state.availability[id]} 
+			datapack={[
+			    this.state.tagSelects, 
+			    this.state.projectSelects, 
+			    this.state.possibleProjects, 
+			    this.state.possibleProjectsRev, 
+			    this.state.possibleTags, 
+			    this.state.possibleTagsRev
+			]}
+			/>
+		    ))}
+
                 </IonContent>
             </IonPage>
         )
