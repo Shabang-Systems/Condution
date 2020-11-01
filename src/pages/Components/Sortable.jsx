ce8866319b0f5fe7060ffd93a1cc26d90ffa5787
Houjun Liu <hliu@shabang.cf>
2020-11-01 13:49:24 -0800

    it's... something

--- src/pages/Components/Sortable.jsx
+++ src/pages/Components/Sortable.jsx
@@ -1,22 +1,44 @@
 import React, { Component } from 'react';
 import { useDrag } from 'react-use-gesture'
-import { useSpring, animated } from 'react-spring'
+import { useSprings, animated, interpolate } from 'react-spring'
 
 import Task from './Task';
 
 const SortableTaskList = (props)=>{
-      const [{ x, y }, set] = useSpring(() => ({ x: 0, y: 0 }))
-  // Set the drag hook and define component movement based on gesture data
-  const bind = useDrag(({ down, movement: [mx, my] }) => {
-    set({ x: down ? mx : 0, y: down ? my : 0 })
-  })
+    const getAnimationDestinationFromIndex = (activeIndex, y) => (indx) => {
+        return activeIndex === indx ? {y, zIndex:1000} : {y: 0, zIndex:0}; // if the index is the one that's being dragged, move up by howevermuch needed
+                                                       // if not, move up by nada
+    }
 
-    return props.list.map(id => {
-        return <animated.div {...bind()} style={{marginBottom: y}} onDragStart={(e)=>e.stopPropagation()}><Task tid={id} key={id+"-"+props.prefix} datapack={props.datapack} uid={props.uid} engine={props.engine} gruntman={props.gruntman} availability={props.availability[id]} /></animated.div>
+    //const [{ x, y }, set] = useSpring(() => ({ x: 0, y: 0 }))
+    const [springs, set, stop] = useSprings(props.list.length, getAnimationDestinationFromIndex(0, 0))
+
+    // Set the drag hook and define component movement based on gesture data
+    const bind = useDrag(({ args: [index], down, movement: [_, my] }) => {
+        set(getAnimationDestinationFromIndex(index, down?my:0))
+    })
+
+    return props.list.map((id, i) => {
+        let anim = springs[i];
+        return (
+            <animated.div 
+                {...bind(i)} 
+                style={{
+                    zIndex: anim.zIndex,  // z-index is 1000 during drag
+                    transform: interpolate([anim.y], (y) => `translate3d(0,${y}px,0)`) // interpolate the transform, b/c that's, uh, the dragging part
+                }} 
+                onClick={(e)=>{
+                    if(anim.zIndex === 1000) // we are being dragged 
+                        e.stopPropagation(); // no clicky!
+                }}
+            >
+                <Task tid={id} key={id+"-"+props.prefix} datapack={props.datapack} uid={props.uid} engine={props.engine} gruntman={props.gruntman} availability={props.availability[id]} />
+            </animated.div>
+    )
     });
 }
 
 export default SortableTaskList;
- 
+
 
 
