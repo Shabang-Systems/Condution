a1347294f865df633f9d2c4a6c3365755acc47c6
Houjun Liu <hliu@shabang.cf>
2020-11-01 19:34:16 -0800

    I mean... It works?

--- src/pages/Components/Sortable.jsx
+++ src/pages/Components/Sortable.jsx
@@ -26,34 +26,32 @@ const SortableTaskList = (props)=>{
 
     const [activelyDragging, setActivelyDragging] = useState([]); // we are actively dragging...
 
-    const order = useRef();
     const moveApplied = useRef();
     const currentIndex = useRef();
 
-    // the order!
+    let order = props.list.map((_, i)=>i);
+
+    // initialize presistant refs
     useEffect(() => {
-        order.current = props.list.map((_, i)=>i); // we start with just [0,1,2...]
         moveApplied.current = 0; // moves applied
         currentIndex.current = 0; // currentIndex
     }, [props.list, props.uid]);
 
 
-    const getAnimationDestinationFromIndex = (activeIndex, y, currentOrder) => (indx) => {
+    const getAnimationDestinationFromIndex = (activeIndex, mY, currentOrder) => (indx) => {
         return activeIndex === indx ?  {
-            y: (currentOrder[indx]-indx)*40 + y, // number of tasks the index is out of place * height of task + cursor movement => correct dragged position offset
+            y: (currentOrder.indexOf(indx)-indx)*40 + mY-((currentOrder.indexOf(indx)-indx)*40), // number of tasks the index is out of place * height of task + cursor movement => correct dragged position offset
             zIndex:1000, 
-            marginTopBottom: 10, 
             config: {tension: 100, friction: 2, mass: 1, clamp: true}
         } : {
-            y: (currentOrder[indx]-indx)*40,  // number of tasks the index is out of place * height of task => correct adjustment to position
+            y: (currentOrder.indexOf(indx)-indx)*40,  // number of tasks the index is out of place * height of task => correct adjustment to position
             zIndex:0, 
-            marginTopBottom: 0
         }; // if the index is the one that's being dragged, move up by howevermuch needed
     }
 
 
     //const [{ x, y }, set] = useSpring(() => ({ x: 0, y: 0 }))
-    const [springs, set, stop] = useSprings(props.list.length, getAnimationDestinationFromIndex(-1, 0, order.current))
+    const [springs, set, stop] = useSprings(props.list.length, getAnimationDestinationFromIndex(-1, 0, order))
 
     // Set the drag hook and define component movement based on gesture data
     const bind = useDrag(({ args: [index], down, movement: [_, movementY] , first, last}) => {
@@ -71,19 +69,18 @@ const SortableTaskList = (props)=>{
         if (Math.abs(moveBy) > 0 && moveBy!==moveApplied.current) {
             // @enquierer crushing @jemoka's hopes and dreams
             let newIndex = index+moveBy;
-            order.current.splice(currentIndex.current, 1); // splice element out
-            order.current.splice(newIndex, 0, index); // splice the index in, noting that we just took something out
-            console.log(currentIndex.current, newIndex, moveBy, order.current);
+            order.splice(currentIndex.current, 1); // splice element out
+            order.splice(newIndex, 0, index); // splice the index in, noting that we just took something out
             moveApplied.current = moveBy;
             currentIndex.current = newIndex;
         }
+        set(getAnimationDestinationFromIndex(index, down?movementY:0, order)) // set the animation function
 
-        set(getAnimationDestinationFromIndex(index, down?movementY:0, order.current)) // set the animation function
 
         if (last) {// if we are done dragging
             setTimeout(()=> setActivelyDragging(activelyDragging.filter(x=>x!==index)), 100); // wait for the lovely event bubble and say we are done
             // TODO probably should also calculate positions + hit the DB
-            set(getAnimationDestinationFromIndex(-1, 0, order.current)) // reset animations
+            set(getAnimationDestinationFromIndex(-1, 0, order)) // reset animations
         }
 
 
@@ -101,7 +98,6 @@ const SortableTaskList = (props)=>{
                     cursor: "pointer",
                     zIndex: anim.zIndex,  // z-index is 1000 during drag
                     transform: interpolate([anim.y], (y) => `translate3d(0,${y}px,0)`), // interpolate the transform, b/c that's, uh, the dragging part
-                    margin: interpolate([anim.marginTopBottom], (tb) => `${tb}px 2px`) // interpolate the margins
                 }} 
                 onClickCapture={(e)=>{
                     if (activelyDragging.includes(i)){ // if we are still dragging
