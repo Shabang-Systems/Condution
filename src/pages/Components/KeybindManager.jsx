73cac834de0835c848e6bd19bf740efafcf91efc
TheEnquirer <huxmarv@nuevaschool.org>
2020-11-05 13:36:33 -0800

    sidebar switching keybinds working!

--- src/pages/Components/KeybindManager.jsx
+++ src/pages/Components/KeybindManager.jsx
@@ -5,72 +5,54 @@ import { Redirect, Route, Link, Switch } from 'react-router-dom';
 import { createBrowserHistory, createHashHistory } from 'history';
 import { withRouter } from "react-router";
 
-
 const autoBind = require('auto-bind/react');
-
-
-
-// HOW SHOULD THE KEYBINDS WORK?
-//
-// Prefix? 
-// Switching sidebar: cmd -1,2,3?
-//
-//
-//
-//
-//
-
-
-
-
 
 
 
 class Keybinds extends Component {
     constructor(props) {
         super(props);
-	//this.stuffur = this.stuffur.bind(this);
         autoBind(this);
 
         this.state = {
 	    sidebar_list: [],
 	    prop_store: ""
 	}
-
-	//this.keybindRef = React.createRef()
-
     }
 
     // define the bindings! 2d array of keybind and function 
     bindings = [
-	["alt+1", ()=>{this.switch_to_upcoming(this)}], // nav to upcoming keybind
-	["alt+3", ()=>{this.perspective_switcher(this, 0)}], // nav to perspective 1 keybind
-	["alt+4", ()=>{this.perspective_switcher(this, 1)}] // nav to perspective 2 keybind
+	["alt+1", ()=>{this.sidebar_switcher(this, 0)}], // nav to upcoming keybind
+	["alt+2", ()=>{this.sidebar_switcher(this, 1)}], // nav to completed keybind
+	["alt+3", ()=>{this.sidebar_switcher(this, 2)}], // nav to item 3 keybind
+	["alt+4", ()=>{this.sidebar_switcher(this, 3)}], // nav to item 4 keybind
+	["alt+5", ()=>{this.sidebar_switcher(this, 4)}], // nav to item 5 keybind
+	["alt+6", ()=>{this.sidebar_switcher(this, 5)}], // nav to item 6 keybind
+	["alt+7", ()=>{this.sidebar_switcher(this, 6)}], // nav to item 7 keybind
+	["alt+8", ()=>{this.sidebar_switcher(this, 7)}], // nav to item 8 keybind
+	["alt+9", ()=>{this.sidebar_switcher(this, 'l')}], // nav to last item keybind
     ]
 
-    switch_to_upcoming(that){
-	console.log(that); 
-	that.props.history.push("/upcoming"); 
-	that.props.paginate("upcoming"); 
-
-    }
-
-
-    perspective_switcher(that, num){
-	//const pid = that.props.perspectives[num].id
-	//console.log(that.props.perspectives[num].id)
-	//that.props.history.push(`/perspectives/${pid}`);
-	//that.props.paginate("perspectives", pid);
-	
-
-	console.log(this.state.sidebar_list)
-
+    sidebar_switcher(that, num){
+	// set our location to index in sidebar, unless you pass it 'l' which switches to the last item 
+	const loca = (num == 'l')? this.state.sidebar_list[this.state.sidebar_list.length-1] : this.state.sidebar_list[num];
+	that.props.history.push(`/${loca[0]}/${loca[1]}`) // push to the history
+	that.props.paginate(...loca); // paginate-ify it!
     }
 
     componentDidUpdate(){
-	console.log(this.props)
-	if (this.state.prop_store != this.props) {
-	    this.setState({sidebar_list: [this.props.perspectives.map(o => ['perspectives', o.id])], prop_store: this.props})
+	// update the props
+	if (this.state.prop_store != this.props) { // if the props have changed, 
+	    this.setState({
+		sidebar_list: [
+		    ['upcoming', ''], // set the first item to upcoming 
+		    // (i could do + but i think thats less efficent 
+		    ['completed', ''], // set the second item to completed
+		    ...this.props.perspectives.map(o => ['perspectives', o.id]), // map the perspectives
+		    ...this.props.projects.map(o => ['projects', o.id]) // and the projects 
+		], 
+		prop_store: this.props //  and update the props 
+	    })
 	}
     }
 
@@ -95,4 +77,5 @@ class Keybinds extends Component {
 }
 
 export default withRouter(Keybinds);
+
 
