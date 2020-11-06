30fc3bde1608f62b843fb9dd9f95e6cb3926c29c
TheEnquirer <huxmarv@nuevaschool.org>
2020-11-05 18:02:03 -0800

    wrapping on sidebar nav keybinds + alt-0

--- src/pages/Components/KeybindManager.jsx
+++ src/pages/Components/KeybindManager.jsx
@@ -15,14 +15,15 @@ class Keybinds extends Component {
         autoBind(this);
 
         this.state = {
-	    sidebar_list: [],
-	    sidebar_index: 0,
-	    prop_store: ''
+	    sidebar_list: [], // what's in the sidebar? 
+	    sidebar_index: 0, // how far down the sidebar are we? 
+	    prop_store: '' // store props so we can check if they have changed 
 	}
     }
 
     // define the bindings! 2d array of keybind and function 
     // TODO TODO: for some reason binding works now, so replace that later? 
+    // TODO: expand these keybinds. ie, n/p and up/down arrows
     bindings = [
 	["alt+1", ()=>{this.sidebar_switcher(this, 0)}], // nav to upcoming keybind
 	["alt+2", ()=>{this.sidebar_switcher(this, 1)}], // nav to completed keybind
@@ -32,21 +33,28 @@ class Keybinds extends Component {
 	["alt+6", ()=>{this.sidebar_switcher(this, 5)}], // nav to item 6 keybind
 	["alt+7", ()=>{this.sidebar_switcher(this, 6)}], // nav to item 7 keybind
 	["alt+8", ()=>{this.sidebar_switcher(this, 7)}], // nav to item 8 keybind
-	["alt+9", ()=>{this.sidebar_switcher(this, this.state.sidebar_list.length-1)}], // nav to last item keybind
-	["alt+j", ()=>{this.sidebar_incrimentor(this, 1)}], // nav to item 8 keybind
-	["alt+k", ()=>{this.sidebar_incrimentor(this, -1)}], // nav to item 8 keybind
+	["alt+9", ()=>{this.sidebar_switcher(this, 8)}], // nav to item 9 keybind
+	["alt+0", ()=>{this.sidebar_switcher(this, this.state.sidebar_list.length-1)}], // nav to last item keybind
+	["alt+j", ()=>{this.sidebar_incrimentor(this, 1)}], // nav down keybind 
+	["alt+k", ()=>{this.sidebar_incrimentor(this, -1)}], // nav up keybind 
     ]
 
     sidebar_switcher(that, num){
 	const loca = this.state.sidebar_list[num]; // set our location to index in sidebar
 	that.props.history.push(`/${loca[0]}/${loca[1]}`) // push to the history
 	that.props.paginate(...loca); // paginate-ify it!
-	this.setState({sidebar_index: num})
+	this.setState({sidebar_index: num}) // update the sidebar index 
     }
 
     sidebar_incrimentor(that, num) {
-	if (this.state.sidebar_list[this.state.sidebar_index+num]) {
-	    this.sidebar_switcher(that, this.state.sidebar_index+num)
+	const next_loca = this.state.sidebar_list[this.state.sidebar_index+num] // store the variable -- we can delet this. 
+	if (next_loca) { // if its defined, 
+	    this.sidebar_switcher(that, this.state.sidebar_index+num) // go there
+	    // if it's not defined and it's at the end, 
+	} else if (this.state.sidebar_index == this.state.sidebar_list.length-1) {
+	    this.sidebar_switcher(that, 0) // wrap around and go back to the beginning 
+	} else { // if it's not defined and not at the end, then it must be at the beginning,
+	    this.sidebar_switcher(that, this.state.sidebar_list.length-1) // so go to the end 
 	}
     }
 
@@ -64,8 +72,10 @@ class Keybinds extends Component {
 		prop_store: this.props //  and update the props 
 	    })
 	}
+
+	// TODO: get the current url and set the index that way 
 	//if (!this.state.sidebar_index) {
-	    //this.setState({sidebar_index: side
+	//    this.setState({sidebar_index: side
 
 	//}
     }
