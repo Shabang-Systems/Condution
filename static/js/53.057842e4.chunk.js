65308719cca544498645fcb28a2348132f53f5a0
Deployment Bot (from Travis CI) <deploy@travis-ci.org>
2020-11-08 07:40:10 +0000

    Deploy Shabang-Systems/Condution to github.com/Shabang-Systems/Condution.git:gh-pages

---
+++ static/js/53.057842e4.chunk.js
@@ -0,0 +1,2 @@
+(this.webpackJsonpCondution=this.webpackJsonpCondution||[]).push([[53],{443:function(n,t,o){"use strict";o.r(t),o.d(t,"startStatusTap",(function(){return i}));var e=o(23),i=function(){var n=window;n.addEventListener("statusTap",(function(){Object(e.j)((function(){var t=n.innerWidth,o=n.innerHeight,i=document.elementFromPoint(t/2,o/2);if(i){var c=i.closest("ion-content");c&&c.componentOnReady().then((function(){Object(e.g)((function(){return c.scrollToTop(300)}))}))}}))}))}}}]);
+//# sourceMappingURL=53.057842e4.chunk.js.map
