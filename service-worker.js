2dc29728494aff09bdbfcaf4b13ed98cf0d2e052
Deployment Bot (from Travis CI) <deploy@travis-ci.org>
2020-11-09 01:01:37 +0000

    Deploy Shabang-Systems/Condution to github.com/Shabang-Systems/Condution.git:gh-pages

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "./precache-manifest.7ecea3e6ebbc67507ff6b103c78db80f.js"
+  "./precache-manifest.87287f1cc30a1aa298e9788ec786e128.js"
 );
 
 self.addEventListener('message', (event) => {
