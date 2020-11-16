0e3b6dc2dea4b3beedd97ee8bd3505166b0ed7b4
Houjun Liu <hliu@shabang.cf>
2020-11-16 08:51:06 -0800

    package updates and the load

--- src/pages/Loader.jsx
+++ src/pages/Loader.jsx
@@ -1,45 +1,36 @@
 import { IonContent, IonPage, IonSplitPane, IonMenu, IonText, IonIcon, IonMenuButton, IonRouterOutlet } from '@ionic/react';
 //import { chevronForwardCircle, checkmarkCircle, filterOutline, listOutline, bicycle } from 'ionicons/icons';
 import React, { Component } from 'react';
+import Lottie from 'react-lottie';
 import './Pages.css';
 
+const LoadSpinner = require('../static/load.json');
+
+console.log(LoadSpinner);
+
 const autoBind = require('auto-bind/react');
+
+const defaultOptions = {
+    loop: true,
+    autoplay: true, 
+    animationData: LoadSpinner,
+    rendererSettings: {
+        preserveAspectRatio: 'xMidYMid slice'
+    }
+};
 
 function Loader(props) {
     return (
         <div style={{width: "100%", height: "100%", position: "absolute", display: "flex", justifyContent: "center", alignItems: "center"}}>
-            <div style={{marginRight: 20, maxWidth: 500}}>
-                <b>Condution v1.0.0-alpha.0</b> <br />
-
-                    <i>Like, why are you reading this?</i> <br />
-
-                    If there is something here [{props.error}], <br />
-                then talk to Jack or something. Or fix it. <br />
-                    INTERNAL USE ONLY. DO NOT DISTRIBUTE.
+            <Lottie options={defaultOptions}
+                height={50}
+                width={50}
+                isStopped={false}
+                isPaused={false}
+            />
+            <div style={{position: "absolute", bottom: 15, color: "var(--decorative-light-alt)", fontSize: 11}}>
+                ©2019-2020 Shabang Systems, LLC and the Condution Authors. Software released under GNU-GPLv3.
             </div>
-            <div style={{marginLeft: 20, maxWidth: 500}}>
-                <br />
-
-                <pre style={{fontSize: 10}}>
-                    {`
-    Copyright (C) 2019-2020. Shabang Systems, LLC
-
-    This program is free software: you can redistribute it and/or modify
-    it under the terms of the GNU General Public License as published by
-    the Free Software Foundation, either version 3 of the License, or
-    (at your option) any later version.
-
-    This program is distributed in the hope that it will be useful,
-    but WITHOUT ANY WARRANTY; without even the implied warranty of
-    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-    GNU General Public License for more details.
-
-    You should have received a copy of the GNU General Public License
-    along with this program.  If not, see https://www.gnu.org/licenses/.
-                    `}
-                </pre>
-            </div>
-
         </div>
     )
 }
