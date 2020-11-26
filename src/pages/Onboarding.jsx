f23d4b913a58ad075e702a6db7d49b3c006313db
Houjun Liu <hliu@shabang.cf>
2020-11-26 15:49:00 -0800

    onboarding interaction [skip travis]

--- src/pages/Onboarding.jsx
+++ src/pages/Onboarding.jsx
@@ -8,35 +8,14 @@ const autoBind = require('auto-bind/react');
 function Onboarding(props) {
     return (
         <div style={{width: "100%", height: "100%", position: "absolute", display: "flex", justifyContent: "center", alignItems: "center", color:"var(--content-normal-accent)"}}>
-            <div style={{marginRight: 20, maxWidth: 500}}>
-                <b>Condution v1.0.0-alpha.0</b> <br />
-                <i>ONBOARDING VIEW!! Woo hoo!</i> <br /> 
-                <span>Arn't you excited to get an account</span> <br /> 
-                <span>at a random todo list app?</span> <br /> 
+            <div style={{background: "var(--background-feature)", marginRight: 40}}>
+                TODO TODO TODO <br /> Pretend that I am a cool animation
             </div>
-            <div style={{marginLeft: 20, maxWidth: 500}}>
-                <br />
-
-                <pre style={{fontSize: 10, color:"var(--content-normal-alt)"}}>
-                    {`
-    Copyright (C) 2019-2020. Shabang Systems, LLC
-
-    This program is free software: you can redistribute it and/or modify
-    it under the terms of the GNU General Public License as published by
-    the Free Software Foundation, either version 3 of the License, or
-    (at your option) any later version.
-
-    This program is distributed in the hope that it will be useful,
-    but WITHOUT ANY WARRANTY; without even the implied warranty of
-    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-    GNU General Public License for more details.
-
-    You should have received a copy of the GNU General Public License
-    along with this program.  If not, see https://www.gnu.org/licenses/.
-                    `}
-                </pre>
+            <div style={{marginRight: 20}}>
+                <b style={{color: "var(--content-normal-alt)", fontSize: 25}}>{props.localizations.setting_up_callout} 🏃</b> <br />
+                {props.localizations.setting_up_action} <br /> 
+                <span style={{fontWeight: 600, marginTop: 20, display: "inline-block"}}>🚄 {props.localizations.setting_up_hold}</span> <br /> 
             </div>
-
         </div>
     )
 }
