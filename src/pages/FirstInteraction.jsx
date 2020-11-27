de3f53141a2b3b6ba6e6665b3db58ceb160b72b0
Houjun Liu <hliu@shabang.cf>
2020-11-26 19:06:44 -0800

    top logo

--- src/pages/FirstInteraction.jsx
+++ src/pages/FirstInteraction.jsx
@@ -4,22 +4,24 @@ import React, { Component } from 'react';
 import './Pages.css';
 import './FirstInteraction.css';
 
-import FIImage from '../static/firstinteraction.png';
+import logo from '../static/logo.png';
 
 const autoBind = require('auto-bind/react');
 
 function FirstInteraction(props) {
     return (
         <div className="first-interaction-container">
-            <div style={{width: "100%", display: "flex", justifyContent: "space-between", alignItems: "flex-end", padding: 10}}>
-                <div className="first-interaction-name">
-                </div>
-
+            <div style={{width: "100%", display: "flex", justifyContent: "space-between", alignItems: "flex-end", flexDirection: "column", padding: 10, flexGrow: 10}}>
+                <div className="first-interaction-logo"><img src={logo} width={25} style={{marginBottom: 1, marginRight: 10, transform: "translateY(-1px)"}}/>Condution Project</div>
                 <div className="first-interaction-callout">
-                    The <br /> Condution  <br /> Project <br />
+                    Tasks? Done.
                 </div>
             </div>
             <div className="first-interaction-actions">
+                <div className="first-interaction-action-container">
+                    <div className="first-interaction-right-callout-a">{props.localizations.onboarding_howdy}</div><div className="first-interaction-right-callout-b">👋 {props.localizations.onboarding_welcome}</div>
+                    <div className="first-interaction-right-callout-c">{props.localizations.onboarding_quick_things}</div>
+                </div>
             </div>
         </div>
     )
