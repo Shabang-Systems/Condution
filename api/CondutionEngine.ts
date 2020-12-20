2b0e269bb173a0c3b0500b62d329c6c285716aba
Houjun Liu <hliu@shabang.cf>
2020-12-19 23:23:38 -0800

    adding env support

--- api/CondutionEngine.ts
+++ api/CondutionEngine.ts
@@ -2,13 +2,15 @@ import Module from './CondutionCore';
 import ReferenceManager from "./Storage/ReferenceManager";
 import FirebaseProvider from "./Storage/Backends/FirebaseBackend";
 
+require('dotenv').config();
 
 async function test() {
     let manager:ReferenceManager = new ReferenceManager([new FirebaseProvider()])
     manager.use("firebase");
 
+    await manager.currentProvider.authenticationProvider.authenticate({payload: {email: process.env.USERNAME, password:process.env.PASSWORD}});
 
-    console.log(await manager.reference("users", "TcZUcte5MFOx410Q8WJ6mRW1Pco1", "tasks").get());
+    let tasks:any = (await manager.reference("users", "TcZUcte5MFOx410Q8WJ6mRW1Pco1", "tasks").get());
 }
 
 test();
