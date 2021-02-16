8be1429c82c60fda040b3a2098d947b6496ea081
zbuster05 <zakary.saya@gmail.com>
2021-02-15 19:22:33 -0800

    Finally made a reasonable new task button

--- src/pages/Components/CalendarTasklistPopover.jsx
+++ src/pages/Components/CalendarTasklistPopover.jsx
@@ -24,7 +24,10 @@ import Task from '../Components/Task';
  *
  */
 
-
+function newTaskButtonClicked() {
+    console.log("amazing");
+}
+// TODO what the hell is this. Why is this not a class.......
 function CalendarTasklistPopover(props) {
 
     let refs = useRef([]);
@@ -33,6 +36,8 @@ function CalendarTasklistPopover(props) {
         refs.current=props.list.map((_)=>createRef());
     }, [props.list]);
 
+
+
     return (
         <IonModal ref={props.reference} isOpen={props.isShown} onDidDismiss={() => {if(props.onDidDismiss) props.onDidDismiss()}} style={{borderRadius: 5}} cssClass="calendar-list-popover">
                 <div id="airplane-hanger"></div> {/*to mount the calendar*/}
@@ -40,7 +45,11 @@ function CalendarTasklistPopover(props) {
                 <span id="calendar-page-header">
                     <div class="calendar-page-count">{props.list.length}</div>
                     <div class="calendar-page-title">tasks due on</div>
-                    <div class="calendar-page-date" >{props.currentDate.toLocaleString('en-us', {  weekday: 'long', year: 'numeric', month: 'long', day: 'numeric'  })}</div>
+                    <div class="calendar-page-date" >{props.currentDate.toLocaleString('en-us', {  weekday: 'long', year: 'numeric', month: 'long', day: 'numeric'  })}
+        
+        </div>                <div className="new-tag-button" onClick={newTaskButtonClicked}>
+                    <i class="fas fa-plus" style={{marginLeft: "2px", marginRight: "2px"}}></i>
+                </div>
                 </span>
 
                 {props.list.map((id, i)=>
@@ -48,6 +57,8 @@ function CalendarTasklistPopover(props) {
                         <Task tid={id} key={id} uid={props.uid} engine={props.engine} gruntman={props.gruntman} availability={props.availability[id]} datapack={props.datapack} envelope={refs.current[i]} onModal={true}/>
                     </div>
                 )}
+
+                
 
 
                 <div className="bottom-helper" style={{height: 150}}>&nbsp;</div>
