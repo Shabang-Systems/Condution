1e128fcafbacfaa64b3411d57041cbea4db85e4d
TheEnquirer <huxmarv@nuevaschool.org>
2021-02-16 20:30:14 -0800

    updated releasenote modal

--- src/pages/Components/ReleaseNotesModal.jsx
+++ src/pages/Components/ReleaseNotesModal.jsx
@@ -54,10 +54,20 @@ function ReleaseNotesModal(props) {
                 <p className="content">Throughout the interface, you will notice that we sprinkled some indicators (heatmaps, progressbars, etc.) of how much a project/group of tasks are completed. The weights of those indicators are driven by user-specificed tag weights!</p>
                 <p className="content">The tag pane <i className="fas fa-tags"></i> accessible from every task allows you to pick tags and set their weights. These serve as the foundation of calculating the weight of a tag (and by extension, a project).</p>
                 <p className="content">A "High Energy" tag, for instance, could have a higher weight; so, tasks tagged with that tag will be weighted more heavily.</p>
+
+                <h1 className="header"> 📅 Task Calendar View </h1>
+		<p className="content"> A new built-in tab comes to Condution! This is a much requested feature by this community. Tasks could now be viewed on a heatmap-style calendar that allows you to see how busy each day is at a glance. </p>
+                <h1 className="header"> 📏 Upcoming Timeline </h1>
+		<p className="content"> We replaced the one-week datebox on the Upcoming view with an infinitely-scrollable timeline! </p>
+		<p className="content"> Tap "show timeline" below the Due-Soon section of Upcoming view and you will see every task with a due date sorted in chronological order. </p>
+                <h1 className="header"> 🛌 Niceties </h1>
+		<p className="content"> We added a plethora of niceties to make the app more fluid and streamlined. </p>
+		<p className="content"> 
+		Just a few of these niceties include: completable projects, sidebar drag & drop, easy navigation with a quick-switcher and key-binds, deletable and editable tags, an intuitive perspective builder, deletable tasks, and so much more!</p> 
                 <div style={{borderTop: "solid 1px var(--background-feature)", width: "100%", height: 1}}>&nbsp;</div>
                 <h1 className="header">🙌 Cheerio</h1>
                 <p className="content">Building Condution has been great fun for all of us; we hope you enjoy the app as well. As always, let us know if you have any feedback <a href="https://discord.gg/3hS7yv3">on Discord</a> or <a href="https://github.com/Shabang-Systems/Condution">on GitHub</a>.</p>
-                <p className="content">If you would like to support our Development, we would be incredibly happy if you would pitch in on <a href="https://www.patreon.com/condution">Patreon</a>. Your contribution will help us keep servers running and features going.</p>
+                <p className="content">If you would like to support our Development, we would be incredibly happy if you would pitch in on Patreon. Your contribution will help us keep servers running and features going.</p>
                 <p className="content">As always, with ❤️  and 🥗, thanks for using Condution.</p>
             </div>
         </IonModal>
