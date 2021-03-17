3082ea34e998ba799637a3221531573aa1ab8f4d
Houjun Liu <hliu@shabang.cf>
2021-03-16 17:11:34 -0700

    test CI desktop

--- decrypt.sh
+++ decrypt.sh
@@ -3,3 +3,5 @@ gpg --quiet --batch --yes --decrypt --passphrase="$SECRETS_PASSPHRASE" --output 
 
 gpg --quiet --batch --yes --decrypt --passphrase="$SECRETS_PASSPHRASE" --output id_rsa .secrets/id_rsa.gpg
 
+gpg --quiet --batch --yes --decrypt --passphrase="$SECRETS_PASSPHRASE" --output electron/.env .secrets/env.gpg
+
