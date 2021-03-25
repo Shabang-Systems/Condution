c74ea86ec3d0feabd45a28bfc4c5e982cd22cc7a
Houjun Liu <hliu@shabang.cf>
2021-03-25 00:19:24 -0700

    commenting out firebase

--- src/CondutionEngine.ts
+++ src/CondutionEngine.ts
@@ -116,10 +116,10 @@ async function test(): Promise<void> {
     let jsprovider: JSONProvider = new JSONProvider("../demo.json", "json", __dirname);
     //provider.commit(data);
     //console.log(provider.load());
-    let fbprovider: FirebaseProvider = new FirebaseProvider("firebase");
-    let manager: ReferenceManager = new ReferenceManager([fbprovider])
+    //let fbprovider: FirebaseProvider = new FirebaseProvider("firebase");
+    let manager: ReferenceManager = new ReferenceManager([jsprovider])
 
-    await fbprovider.authenticationProvider.authenticate({ payload: { email: process.env.USERNAME, password: process.env.PASSWORD } });
+    //await fbprovider.authenticationProvider.authenticate({ payload: { email: process.env.USERNAME, password: process.env.PASSWORD } });
 
     //// --- TODO everything called before this line needs to be refactored... :( ---
 
@@ -128,14 +128,14 @@ async function test(): Promise<void> {
     await cm.start(); // start our context
 
 
-    console.log("we are here");
+    //console.log("we are here");
     //console.log(p.simpleGroups[0].filters);
-    let testPersp:Perspective = await Perspective.fetch(cm, "r3dQARV8E4w7h4EnBDX5");
-   //let tasktest = await Task.fetch(cm, "y0ptg4qd05efgb9dp74sqp9sin503npn");
-    console.log("we are here2");
-    console.log(testPersp.query);
-    console.log(await testPersp.execute());
-    console.log("done");
+//    let testPersp:Perspective = await Perspective.fetch(cm, "r3dQARV8E4w7h4EnBDX5");
+   ////let tasktest = await Task.fetch(cm, "y0ptg4qd05efgb9dp74sqp9sin503npn");
+    //console.log("we are here2");
+    //console.log(testPersp.query);
+    //console.log(await testPersp.execute());
+    //console.log("done");
     //tasktest.name = "apple!";
     //console.log(await testPersp.execute());
     //let perspective2:Perspective = await Perspective.create(cm, "Electric Boogaloo");
