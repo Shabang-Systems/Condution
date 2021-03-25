5800d1f0e1d7896211ad033a0b22076bbc9bf977
Houjun Liu <hliu@shabang.cf>
2021-03-25 15:07:15 -0700

    ah welp

--- src/CondutionEngine.ts
+++ src/CondutionEngine.ts
@@ -82,9 +82,13 @@ import JSONProvider from "./Storage/Backends/JSONBackend";
  * except, of course, if you got blocked server-side. This also means that you 
  * could delete whole users (if you have perms) and mess up the DB. So have fun!
  *
+ * To use ReferenceManagers, instantiate with instances of the Providers that you
+ * want to setup. Then, call .use on the name on the provider's name that you want
+ * to use. Finally, query for a reference as shown below.
+ *
  * > let manager = new ReferenceManager([new FirebaseProvider(), new JSONProvider()]);
  * > manager.use("firebase");
- * > manager.reference("users, "test", "tasks", "434d5fab10129a").get();
+ * > manager.reference(["users, "test", "tasks", "434d5fab10129a"]).get();
  *
  * And kaboom you get an object of task by user `test` by ID `434d5fab10129a`.
  *
@@ -92,153 +96,65 @@ import JSONProvider from "./Storage/Backends/JSONBackend";
 
 import ReferenceManager from "./Storage/ReferenceManager";
 
+/*
+ * Chapter 3: The Context
+ *
+ * This is THE most important piece of thing that may be confusing. Contexts are
+ * a layer above managers that allow you to actually query for specific pages by
+ * users, manage workspaces, and get data using a similar syntax as does Managers 
+ * but without the confusing confusal of needing the know the UID, worry about workspaces
+ * etc.
+ *
+ * To start, pass a manager to the Context to instantiate it. Then, run the async start
+ * function to load workspaces and chains.
+ *
+ * > let cm: Context = new Context(manager); // create the context
+ * > await cm.start()
+ * > await cm.reference(["tasks", "434d5fab10129a" ]).get()
+ *
+ */
 
+import { Context } from "./Objects/EngineManager";
 
+/*
+ * Chapter 4: The Objects
+ *
+ * Here are all the objects that Condution could manage for you! Tasks, Tags, Projects
+ * all the rage! Unfortunately I am too swollen to type up docs at the moment, so
+ * TODO and just look in the inline documentation for yourself
+ *
+ */
 
 import Workspace from "./Objects/Workspace";
-import Task from "./Objects/Task";
-import Tag from "./Objects/Tag";
-import Project from "./Objects/Project";
-import { RepeatRule, RepeatRuleType, Query } from "./Objects/Utils";
-import { Context } from "./Objects/EngineManager";
-//import { Collection, Page } from "./Storage/Backends/Backend";
-
+import Task, { TaskSearchAdapter } from "./Objects/Task";
+import Tag, { TagSearchAdapter } from "./Objects/Tag";
+import Project, { ProjectSearchAdapter } from "./Objects/Project";
 import Perspective, { PerspectiveQuery, AvailabilityTypes, OrderTypes } from "./Objects/Perspective";
 
-require('dotenv').config();
+/*
+ * Chapter 5: Utilities
+ *
+ * Bugger that I am bleeding now
+ */
 
-//// #!/Shabang | Condution at home:
-//// - Missing collections/pages are not handled well
-//// 
+import { RepeatRule, RepeatRuleType, Query, GloballySelfDestruct } from "./Objects/Utils";
+import type { AdapterData, Filterable } from "./Objects/Utils";
 
+let Internal:object = { Collection, Page, Provider, ReferenceManager }
+let Repeat:object = { RepeatRule, RepeatRuleType }
+let Utilities:object = { GloballySelfDestruct }
 
-async function test(): Promise<void> {
-    let jsprovider: JSONProvider = new JSONProvider("../demo.json", "json", __dirname);
-    //provider.commit(data);
-    //console.log(provider.load());
-    //let fbprovider: FirebaseProvider = new FirebaseProvider("firebase");
-    let manager: ReferenceManager = new ReferenceManager([jsprovider])
+export {
+    Internal,
+    Context,
+    Repeat,
+    Utilities,
+    Workspace, 
+    Task, TaskSearchAdapter,
+    Tag, TagSearchAdapter,
+    Project, ProjectSearchAdapter, Query,
+    Perspective, PerspectiveQuery, AvailabilityTypes, OrderTypes
+};
 
-    //await fbprovider.authenticationProvider.authenticate({ payload: { email: process.env.USERNAME, password: process.env.PASSWORD } });
+export type { AdapterData, Filterable };
 
-    //// --- TODO everything called before this line needs to be refactored... :( ---
-
-    let cm: Context = new Context(manager); // create the context
-    //cm.useProvider("firebase");
-    await cm.start(); // start our context
-
-
-    //console.log("we are here");
-    //console.log(p.simpleGroups[0].filters);
-    //let testPersp:Perspective = await Perspective.fetch(cm, "r3dQARV8E4w7h4EnBDX5");
-    //console.log(await testPersp.execute())
-//    testPersp = await Perspective.fetch(cm, "r3dQARV8E4w7h4EnBDX5");
-    //testPersp = await Perspective.fetch(cm, "r3dQARV8E4w7h4EnBDX5");
-    //testPersp = await Perspective.fetch(cm, "r3dQARV8E4w7h4EnBDX5");
-   ////let tasktest = await Task.fetch(cm, "y0ptg4qd05efgb9dp74sqp9sin503npn");
-    //console.log("we are here2");
-    //console.log(testPersp.query);
-    //console.log(await testPersp.execute());
-    //console.log("done");
-    //tasktest.name = "apple!";
-    //console.log(await testPersp.execute());
-    //let perspective2:Perspective = await Perspective.create(cm, "Electric Boogaloo");
-    //testPersp.query = "[.woha !.no #hewo12] [!#how] [#goes .it] ($519913 > [.woha #hewo12]$due)";
-    //console.log(testPersp.tempdata);
-    //console.log(testPersp.availability == AvailabilityTypes.REMAIN);
-    //console.log("we are here");
-    //let queryEngine:Query = new Query(cm);
-    //console.log("we are here3");
-    //let allofeveryperspective:Perspective[] = await queryEngine.execute(Perspective, (_:Perspective)=>true) as Perspective[];
-    //console.log("we are here4");
-    //console.log(allofeveryperspective[2]);
-    //console.log("DONE::::", (await allofeveryperspective[2].execute()).length);
-    //allofeveryperspective.map((p:Perspective) => console.log(p.id, p.name));
-    //let p:PerspectiveQuery = new PerspectiveQuery(cm, "[.woha !.no #hewo12] [!#how] [#goes .it] ($519913 > [.woha #hewo12]$due)");
-    //console.log(await p.execute());
-   //    @lb's grand vision
-       //sourcesfilters => condition-based sorted set queried based on data. Deals with one condition
-           //Projects + tags => reverse table (and foward table.) static in object
-           
-       //agg => logic??
-       
-      
-    //console.log(tasktest.name);
-   //tasktest.name = "VERY LARGE STRING SO THAT I COULD NOTICE THIS";
-   //console.log(tasktest.name);
-
-    //let tg0:Tag = await Tag.create(cm, "hewo12", 12);
-    //let tg1:Tag = await Tag.fetch(cm, "wtTISSFQylNpeZI5xosX");
-    ////tg.weight = 3;
-    ////console.log(tg.id);
-
-    //let task:Task = await Task.create(cm, "another", null, [tg0]);
-    ////task.tags = [tg1];
-    //console.log(task.weight);
-    //console.log(task.name);
-    ////console.log(await task.calculateWeight())
-
-    ////task.inboxify();
-    ////console.log(task.name);
-
-    ////let a:Date = new Date();
-    //console.log(manager.currentProvider);
-    //let proj:Project = await Project.fetch(cm, "HxgywDvXypqaatjnFsFV");
-//    let proj:Project = await Project.create(cm, "NO SLOW!");
-    //task.move(proj);
-    //console.log(proj.available);
-    //proj.uncomplete();
-    //cm.useProvider("json");
-    //let proj:Project = await Project.fetch(cm, "HxgywDvXypqaatjnFsFV");
-    //console.log(proj.children[0].databaseBadge);
-    //let proj1:Project = await Project.fetch(cm, "ri3c5bssrwb29eptavlbnfs87pzsf141f");
-    //let proj2:Project = await Project.fetch(cm, "ri3c5bssrwb29eptavlbnfs87pzsf141f");
-    //console.log(proj1.name);
-    ////let proj1:Project = await Project.fetch(cm, "ri3c5bssrwb29eptavlbnfs87pzsf141f");
-    //task.move(proj1);
-    //console.log(task.available);
-    ////console.log(task.id);
-    //let b:Date = new Date();
-
-
-    //console.log(task.available)
-    //console.log(proj1.available);
-
-    //console.log(task.available)
-
-   // let q:Query = new Query(cm); // create a query 
-    ////await q.index(); // O(n) through the whole damn database to cache parametres. 
-                     //// this is expensive, so don't do it too much
-    //console.log((await q.execute( // O(n) or O(logn) to find things. depending on what data you are filtering by.
-         //Project,
-        //(i:Project) => i.name=="woha!", // like, name!
-    //))[0].name);
-
-    //console.log(q.execute());
-
-    //console.log(proj.name);
-    //console.log(proj.available);
-    //console.log(proj1.weight);
-    //console.log(proj.name);
-    //await task.inboxify();
-    //await task.move(proj);
-    //await proj.move(proj1);
-    //await proj.bringToTop();
-    //console.log(proj1.weight);
-
-    //const actualtask:Task = await Task.fetch(cm, "Yuc9B6VuWNbQbbxAlnWp");
-    //await actualtask.complete();
-    //console.log(actualtask.completeDate.toLocaleString());
-    //console.log(actualtask.isComplete);
-    //actualtask.tags = [tg, tg, tg, tg, tg, tg, tg];
-    //let weight:number = await actualtask.calculateWeight();
-    //let wtf = await actualtask.async_tags;
-    //console.log(wtf);
-    //console.log(weight);
-    //actualtask.tags = actualtask.tags.concat([tg]);
-    //console.log("Done! Exit me now.");
-}
-
-test();
-
-//export { ReferenceManager, FirebaseProvider, Task, Workspace, Tag, Project, Context, RepeatRule, RepeatRuleType, Collection, Page };
